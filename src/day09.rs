1c5bf7785eac579e9449693391ef4bd7cec4c55e
Michael Braun <m.braun92@gmail.com>
2023-12-15 01:26:27 +0100

    day9

--- src/day09.rs
+++ src/day09.rs
@@ -14,6 +14,53 @@ pub fn run() {
     }
 }
 
-fn part_1(_lines: &Vec<String>) -> () {}
+fn parse(lines: &Vec<String>) -> Vec<Vec<i64>> {
+    return lines
+        .iter()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|entry| entry.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect();
+}
 
-fn part_2(_lines: &Vec<String>) -> () {}
+fn process_next(data: &Vec<i64>) -> i64 {
+    let last = *data.last().unwrap();
+    let differences: Vec<i64> = data.windows(2).map(|w| w[1] - w[0]).collect();
+
+    if data.windows(2).all(|w| w[0] == w[1]) {
+        let last_diff = *differences.last().unwrap();
+        return last + last_diff;
+    }
+
+    let next = process_next(&differences);
+    return last + next;
+}
+
+fn part_1(lines: &Vec<String>) -> () {
+    let data_sets = parse(lines);
+
+    let sum: i64 = data_sets.iter().map(|v| process_next(v)).sum();
+    println!("Part 1: {}", sum);
+}
+
+fn process_prev(data: &Vec<i64>) -> i64 {
+    let first = *data.first().unwrap();
+    let differences: Vec<i64> = data.windows(2).map(|w| w[1] - w[0]).collect();
+
+    if data.windows(2).all(|w| w[0] == w[1]) {
+        let first_diff = *differences.first().unwrap();
+        return first - first_diff;
+    }
+
+    let prev = process_prev(&differences);
+    return first - prev;
+}
+
+fn part_2(lines: &Vec<String>) -> () {
+    let data_sets = parse(lines);
+
+    let sum: i64 = data_sets.iter().map(|v| process_prev(v)).sum();
+    println!("Part 2: {}", sum);
+}
